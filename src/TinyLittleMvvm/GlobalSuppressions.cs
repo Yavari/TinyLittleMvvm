2178de552462260d207cc02eeb8fadd931c1757c
Payam Yavari <payam@yavtech.se>
2018-06-03 23:10:06 +0200

    Update nuget packages and fixed error warnings

---
+++ src/TinyLittleMvvm/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CC0063:Your Uri syntax is wrong.", Justification = "<Pending>", Scope = "member", Target = "~M:TinyLittleMvvm.FlyoutManager.ShowFlyoutInternal(TinyLittleMvvm.IDialogViewModel)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CC0063:Your Uri syntax is wrong.", Justification = "<Pending>", Scope = "member", Target = "~M:TinyLittleMvvm.DialogManager.ShowDialogAsync``1(TinyLittleMvvm.DialogViewModel{``0},MahApps.Metro.Controls.Dialogs.MetroDialogSettings)~System.Threading.Tasks.Task{``0}")]
+
